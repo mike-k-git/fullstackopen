68fecd425fa5bd1a86815f13177557ac3f84abcd
mike-k-git <mikehsm@outlook.com>
2022-04-08 00:28:32 +0300

    1.6: unicafe step1

---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h2>Give feedback</h2>
+      <button onClick={() => setGood(good + 1)}>good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <h2>Statistics</h2>
+      <p>
+        good {good}
+        <br />
+        neutral {neutral}
+        <br />
+        bad {bad}
+      </p>
+    </div>
+  )
+}
+
+export default App
